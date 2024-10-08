074122eca90713f2ee87362c8b7c5611a0bfd55f
eshihanda <eshihandamonn@gmail.com>
2024-10-08 21:54:43 +0300

    herosection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -4,16 +4,22 @@ import heroImg from '../assets/hero-image.png';
 
 const HeroSection = () => {
   return (
-	<section className='hero-section bg-secondary'>
-		<div className='row justify-content-center align-items-center hero-content'>
-			<div className='col-md-5 text-center text-md-start'>
-				<h1 className='display-4 fw-bold hero-title'> Find Most Suitable Houses</h1>
-				<p className='lead mt-4 hero-description'>
-				Find a variety of houses that suit you easily and forget about the difficulties of finding a house for you.
-				</p>
-			</div>
-			<div className='hero-image'>
-				<img src={heroImg} alt="House" />
+	<section className='hero-section bg-dark mt-5'>
+		<div className='container-lg'>
+			<div className='row justify-content-center align-items-center hero-content'>
+				<div className='col-md-5 text-center text-md-start'>
+					<div className='hero-title'>
+						<h1 className='display-4 text-white fw-bold hero-title'> Find Most <br /> 	Suitable Houses</h1>
+					</div>
+					<div className='hero-description'>
+						<p className='lead mt-4 text-white'>
+						Find a variety of houses that suit you easily and forget about the difficulties of finding a house for you.
+						</p>
+					</div>
+				</div>
+				<div className='col-md-5 text-center hero-image d-none d-md-block img-container'>
+					<img className='img-fluid' src={heroImg} alt="House" />
+				</div>
 			</div>
 		</div>
 	</section>
